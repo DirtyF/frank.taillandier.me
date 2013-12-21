628be7c25baa8224ccc3a340980b8add947922e6
Frank Taillandier <frank.taillandier@gmail.com>
2013-12-21 10:28:18 +0100

    add margin to avatar

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 				<section id="content">
 					<section id="about">
 						<h2>About</h2>
-						<img itemprop="image" class="img-rounded pull-left media-object" src="img/frank-taillandier.jpg" width="120" alt=""/>
+						<img itemprop="image" class="img-rounded pull-left media-object avatar" src="img/frank-taillandier.jpg" width="120" alt=""/>
 						<p itemscope itemtype="http://schema.org/Person" class="vcard media">
 							Hi, my name is
 							<span itemprop="name" class="fn">Frank Taillandier</span>
